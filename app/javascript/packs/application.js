a28de201d2ed49facf89c81b833c523202482328
John Nkp <john.nkp1@gmail.com>
2021-03-03 07:18:03 +1100

    implemented day and night mode via javascritp, created mobile view, added resume

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,89 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+window.onload=function(){
+    
+
+    // Landing page Down arrow
+    const scrollDown = document.querySelector('.down-arrow')
+
+    scrollDown.addEventListener('click', () => {
+        if (document.body.scrollTop < 100) {
+            window.scrollBy(0, 750);
+        }else{
+            return "nothing"
+        }
+    })
+
+    // Landing page Down arrow
+    const projectsBtn = document.querySelector('.project-btn')
+
+    projectsBtn.addEventListener('click', () => {
+        if (document.body.scrollTop < 100) {
+            window.scrollBy(0, 750);
+        }else{
+        return "nothing"
+        }
+    })
+
+    
+    // Day and night button 
+    const dayNightBtn = document.getElementById('day-night-btn')
+    const navBar = document.getElementById('home-header')
+    const navLinks  = document.querySelectorAll('.menu-li')
+    const socialLinks  = document.querySelectorAll('.fab')
+    const logo = document.querySelector('.hero-text')
+    const resume = document.querySelector('.resume-button')
+    const allLinks = document.querySelectorAll('a')
+
+    // To refresh the javascript on the page each time a link is clicked 
+    allLinks.forEach((link) => {
+        link.addEventListener('click', () =>{
+            window.location.reload()
+        })
+    })
+    
+    const arr = [logo, resume]
+
+    dayNightBtn.addEventListener('click', () =>{
+        
+        // for dark mode styling
+        if(dayNightBtn.classList[0] == "light"){
+            
+            dayNightBtn.classList.remove('light')
+            dayNightBtn.classList.add('dark')
+
+            navBar.style.backgroundColor = 'white';
+            arr.forEach((item) => {
+                item.style.color = 'black'
+            })
+            navLinks.forEach((link) => {
+                link.style.color = 'black'
+            })
+            socialLinks.forEach((link) => {
+                link.style.color = 'black'
+            })
+        }
+        // for light mode styling
+        else if(dayNightBtn.classList[0] == "dark") {
+            dayNightBtn.classList.remove('dark')
+            dayNightBtn.classList.add('light')
+
+            navBar.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+            arr.forEach((item) => {
+                item.style.color = '#fff'
+            })
+            navLinks.forEach((link) => {
+                link.style.color = '#fff'
+            })
+            socialLinks.forEach((link) => {
+                link.style.color = '#fff'
+            })
+
+        }
+    })
+
+
+}
